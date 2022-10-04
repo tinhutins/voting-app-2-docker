ef9420f3129c9db8e603fbf3edc61bc34dd822de
tinhutins <tino.hutinski@gmail.com>
2022-10-04 11:54:48 +0200

    tetsts

--- vote/app.py
+++ vote/app.py
@@ -8,7 +8,7 @@ import json
 option_a = os.getenv('OPTION_A', "Nano-3")
 option_b = os.getenv('OPTION_B', "Vim-3")
 hostname = socket.gethostname()
-version = 'v3'
+version = 'v4'
 
 app = Flask(__name__)
 

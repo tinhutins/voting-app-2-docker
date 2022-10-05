8ce86ce8c982e587a818511a2a8b496888f6a4f3
tinhutins <tino.hutinski@gmail.com>
2022-10-05 14:07:44 +0200

    testst

--- e2e.sh
+++ e2e.sh
@@ -1,17 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
-cd e2e 
+cd e2e
 
-docker-compose down > /dev/null 2>&1 
+docker-compose down > /dev/null 2>&1
 
-#sleep 10
+sleep 10
 
 docker-compose build
-docker-compose up -d 
+docker-compose up -d
 
 docker-compose ps
-
-docker-compose run --rm e2e
-
-docker-compose down 
-
